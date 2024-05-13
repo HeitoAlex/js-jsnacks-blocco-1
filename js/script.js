19c94a09f1375cc002039db5a2c4f1238bd8833a
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-13 11:50:30 +0200

    snack 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const userNumberA = prompt('scegli un numero')
+const userNumberB = prompt('scegli un altro numero')
+
+if{
+    userNumberA > userNumberB
+    console.log(userNumberA)
+} else{
+    console.log(userNumberB)
+}
