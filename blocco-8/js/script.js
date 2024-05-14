59f5253ef28df1734f980868fb78ccdcc80813a7
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-14 12:23:31 +0200

    blocco 8 risolto

--- blocco-8/js/script.js
+++ blocco-8/js/script.js
@@ -1,17 +1,19 @@
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
 const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const bodyElement = document.querySelector('body')
 
 for (let i = 0; i < numberList.length; i++){
+    
     if (numberList[i] % 2 === 1){
         const oddDivElement = document.createElement('div');
-        oddDivElement = {color: 'red'};
+        oddDivElement.style.color = "red";
         oddDivElement.append(numberList[i]);
-        body.appendChild(oddDivElement);
+        bodyElement.appendChild(oddDivElement);
     } else {
         const evenDivElement = document.createElement('div');
-        evenDivElement = {color: 'green'};
+        evenDivElement.style.color = "green";
         evenDivElement.append(numberList[i]);
-        body.appendChild(evenDivElement);
+        bodyElement.appendChild(evenDivElement);
     }
 }
