079d240ab0bdccc6c1dcedabbaba6d2223cccfa3
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-14 11:27:50 +0200

    blocco 7

---
+++ blocco-7/js/script.js
@@ -0,0 +1,11 @@
+// Fai inserire un numero, che chiameremo N, all'utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
+
+
+const userNumber = prompt('Inserisci un numero')
+
+for (let i = 0; i < userNumber; i++){
+
+    
+}
+
+
